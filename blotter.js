022ac9d9410aca01f6757edcc902eb2eb351b60c
Gareth Down <g@garethdown.com>
2016-03-27 15:22:16 +0100

    add team trades (sends out 1000 trades on subscription)

--- blotter.js
+++ blotter.js
@@ -5,5 +5,19 @@ module.exports = function(blotter, io) {
     blotter.subscribe(function(position) {
       socket.emit('position', position);
     });
+
+    for (var i = 0; i < 1000; i++) {
+
+      var trade = {
+        date: new Date(),
+        ccyCpl: 'EURUSD',
+        rate: 1.2,
+        notional: 1000000 * i,
+        status: 'done',
+        direction: 'buy'
+      };
+
+      socket.emit('teamTrade', trade);
+    }
   });
 }
