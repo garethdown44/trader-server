04e92c325bb119210d0448ec45a94ceb0193a9cc
Gareth Down <g@garethdown.com>
2016-08-03 17:20:15 +0100

    introduce request/response for team trades

--- team-trades.js
+++ team-trades.js
@@ -1,6 +1,7 @@
 var Rx = require('rx');
 var rand = require('./random');
 var teamTrades = new Rx.ReplaySubject(10000);
+var debug = require('debug')('trader:team-trades');
 
 var ccys = [];
 
@@ -11,16 +12,19 @@ ccys.push('GBPCHF');
 ccys.push('AUDUSD');
 ccys.push('EURHKD');
 
+var trades = [];
+
 for (var i = 0; i < 1000; i++) {
   var trade = {
     date: new Date(),
     ccyCpl: ccys[rand(0,5,0)],
     rate: rand(1.2, 1.3, 2),
-    notional: 1000000 * i,
+    notional: 100000 * rand(1,100,0),
     status: 'done',
     direction: 'buy'
   };
 
+  trades.push(trade);
   teamTrades.onNext(trade);
 }
 
@@ -29,6 +33,18 @@ module.exports = function(blotter, io) {
 
     teamTrades.subscribe(x => {
       socket.emit('teamTrade', x);
+    });
+
+    socket.on('request', function(req) {
+
+      debug('request', req);
+      console.log(req);
+
+      var slice = trades.slice(req.startRow, req.endRow);
+
+      debug('sending back ' + slice.length + ' rows');
+
+      socket.emit('response', slice);
     });
   });
 }
